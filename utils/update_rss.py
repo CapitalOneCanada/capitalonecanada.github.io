ccdf4230961c3ce2cbaf1aa70f480d2d4838772b
Alice Poon <alice.poon@capitalone.com>
2016-04-29 13:22:09 -0400

    Updated script for multiple environments

--- utils/update_rss.py
+++ utils/update_rss.py
@@ -3,11 +3,36 @@
 import subprocess
 from datetime import datetime
 import string
+import sys, os
 
+# Command line arguments for build environment
+
+if len(sys.argv) >= 2:
+    arg = sys.argv[1]
+else:
+    arg = "default - external/prod"
+
+if arg == "internal":
+    url = os.environ["INTERNAL"]
+elif arg == "localhost":
+    url = "localhost:4000"
+else:
+    url = "tech.capitalone.ca"
+
+print "[INFO] Using build environment %s" % arg
+print "[INFO] Checking URL %s" % url
+
+# Setup
 needbuild = False
 now = datetime.now()
-fetchDates = subprocess.check_output("curl localhost:4000/events_feed.xml --silent | grep endDate | sed 's#<[^>]*>##g' | sed 's# [+-].*$#\&#' | xargs", shell=True)
+
+command = 'curl %s/events_feed.xml --silent | grep endDate | sed \'s#<[^>]*>##g\' | sed \'s# [+-].*$#\&#\' | xargs' % url
+
+fetchDates = subprocess.check_output(command, shell=True)
+
 eventDates = fetchDates.split("&")
+
+# Check Event Dates
 
 for d in eventDates:
     d = d.strip()
@@ -15,17 +40,21 @@ for d in eventDates:
         date_object = datetime.strptime(d, '%a, %d %b %Y %H:%M:%S')
         # Sample Date Format: Thu, 24 Mar 2016 20:00:00 +0000
         if now > date_object:
-            print "Event has passed\n[INFO] Current Date:", now, "Event Date:", date_object
+            print "[INFO] Event has passed"
+            print "[INFO] Current Date:", now, "Event Date:", date_object
             needbuild = True
             break
         else:
-            print "Event is in the future\n[INFO] Current Date:", now, "Event Date:", date_object
+            print "[INFO] Event is in the future"
+            print "[INFO] Current Date:", now, "Event Date:", date_object
+
+# Build if needed
 
 if needbuild:
-    print "There is an event that has passed. Will update RSS feeds."
-    print "Commiting"
+    print "[INFO] There is an event that has passed. Will update RSS feeds."
+    print "[INFO] Commiting"
     subprocess.call("git commit -m 'Rebuilding site to update RSS feeds' --allow-empty")
-    print "Pushing to github.com/capitalonecanada/capitalonecanada.github.io"
+    print "[INFO] Pushing to github.com/capitalonecanada/capitalonecanada.github.io"
     subprocess.call("git push origin dev:dev")
 else:
-    print "No rebuild needed. Commit was not made."
+    print "[INFO] No rebuild needed. Commit was not made."
