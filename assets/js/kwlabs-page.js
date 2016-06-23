15fdc25f01abe8930c11d5d38cc037f3e1de0bdc
Han, Ethan <Ethan.Han@capitalone.com>
2016-06-23 14:28:54 -0400

    Added biography lightboxes to /kwlabs. Implemented slide-down-to-close lightbox. Updated some kwlabs image assets.

--- assets/js/kwlabs-page.js
+++ assets/js/kwlabs-page.js
@@ -15,45 +15,103 @@ var employees = [
 		"name":"Joan Qiu",
 		"role":"Senior Data Scientist",
 		"iconURL":baseURL + "/assets/img/kwlabs/JB19766_KW-Joan.png",
-    "biography1":"",
-    "biography2":""
-	},
+    "biography1":"My role is to guide the projects from beginning to end&nbsp;– to act as liaison with the Capital One head office and to provide necessary support for our co-op students. It’s amazing to play such an important role in our digital journey.",
+    // italicize 1989
+    "biography2":"I bring a lot of positive energy to the team. And I love to code while listening to Taylor Swift’s music, especially \"1989\"."
+  },
 	{
 		"name":"Steve Frensch",
 		"role":"Senior Manager",
 		"iconURL":baseURL + "/assets/img/kwlabs/JB19766_KW-Steve.png",
-    "biography1":"",
-    "biography2":""
+    "biography1":"My role is to take care of all areas of the Lab&nbsp;– I manage the team, project development, industry relationships and even the office supplies! However, my goal is to bring out the best in people so they can deliver the best work.",
+    "biography2":"Outside of work, I spend most of my time playing with my kids. My seven-month-old daughter especially enjoys the ‘slap-dad-in-the-face’ game. And she’s really good at it!"
 	},
 	{
 		"name":"Faisal Dosani",
 		"role":"Data Scientist, Manager",
 		"iconURL":baseURL + "/assets/img/kwlabs/JB19766_KW-Faisal.png",
-		"biography1":"",
-		"biography2":""
+		"biography1":"I think I bring some wisdom to the team&nbsp;– I look at data and use technology to tell stories that will help us achieve our mission. At the Lab, we have the freedom to experiment and the freedom of time. We can be fearless.",
+		"biography2":"Few people know that I lived in Hong Kong for 15 years when I was a kid. The best part of my time there was when I participated in a game show on TV ... and won!"
 	},
 	{
 		"name":"David Tsenter",
 		"role":"Co-op Student",
 		"iconURL":baseURL + "/assets/img/kwlabs/JB19766_KW-David.png",
-		"biography1":"",
-		"biography2":""
+		"biography1":"This is my 1st co-op placement. I really enjoy working with a small team, all the while learning the development cycles of a big company like Capital One. I like that the work we do here matters!",
+		"biography2":"My twin sister is two minutes younger than me. If I can’t win an argument with her, I say, \"Respect your elders.\" Surprisingly, it works sometimes ..."
 	},
 	{
 		"name":"Jasmine Ren",
 		"role":"Co-op Student",
 		"iconURL":baseURL + "/assets/img/kwlabs/JB19766_KW-Jasmine.png",
-		"biography1":"",
-		"biography2":""
+		"biography1":"This is my 3rd co-op placement. I was really excited about the prospect of working at the Capital One Lab as I had friends who told me that during their previous assignments here, they were really able to hone their skills. I study statistics and this co-op experience is giving me a powerful insight into how to use math in a business application. I now have a better understanding of what skills I should learn for my career development.",
+		"biography2":"Funny story, while in my 1st year of University&nbsp;– in between moving from one residence to another&nbsp;– I crashed in the lounge of the math building. I did this for three whole days."
 	},
 	{
 		"name":"Eric Hardy",
 		"role":"Co-op Student",
 		"iconURL":baseURL + "/assets/img/kwlabs/JB19766_KW-Eric.png",
-		"biography1":"",
-		"biography2":""
+		"biography1":"This is my 5th co-op placement. I study computer science and I’m into big data, so the Capital One Lab is the perfect opportunity for me. The infrastructure is great&nbsp;– it’s nice to collaborate with the Toronto office, yet we still work independently. It’s inspiring to know that the company will actually use our work.",
+		"biography2":"For a month or so, I’ve been teaching a robot how to play Mario Bros. Hmm, I just realized Mario has a name, but my robot doesn’t. Mario-nette?"
 	},
 ];
+
+/*
+Handle swipe motions to close the profile lightboxes. Note: vertical swipe down to close
+Variable yCoord is used to check y-axis swipe motions
+Function handleTouchStart(e) registers the y-coordinate at the beginning of swipe.
+Funciton handleTouchMove(e) checks the new y-coordinates, and executes the commands depending on what swipe was done
+*/
+var yCoord = null;
+var xCoord = null;
+var yDelta = 0;
+var swipedClosed = false;
+var didSwipe = false;
+var enableSwipeToClose = false;
+
+function handleTouchStart(e) {
+  // flag to check if the lightbox is fully scrolled-to-top
+  // if not, then don't enable swipe-down-to-close
+  enableSwipeToClose = $(".profile-lightbox.show").scrollTop() == 0;
+  yCoord = e.originalEvent.touches[0].pageY;
+  xCoord = e.originalEvent.touches[0].pageX;
+}
+
+function handleTouchMove(e) {
+  if (!enableSwipeToClose || !yCoord || $(".profile-lightbox.show").length == 0) { // do nothing if yCoord is not originally set or not lightbox open
+    return;
+  } else {
+    $(".profile-lightbox.show").addClass("disable-transition");
+    var yNew = e.originalEvent.touches[0].pageY;
+    yDelta = yCoord - yNew;
+		didSwipe = yDelta != 0 || xCoord - e.originalEvent.touches[0].pageX != 0;
+    if (yDelta < 0) { // is swipe down
+			$(".profile-lightbox.show").css("overflow", "hidden");
+      $(".profile-lightbox.show").css("top", 10 + yDelta * -1);
+      swipedClosed = yDelta < -125;
+    }
+  }
+}
+
+function handleTouchEnd(e) {
+	if (didSwipe) {
+	  if (swipedClosed) {
+	    swipedClosed = true;
+	    $(this).addClass("hide").removeClass("show");
+	    $(this).attr("style", "");
+	    resetBackground();
+			$(".profile-lightbox").removeClass("disable-transition");
+	  } else { // reset lightbox position
+			$(".profile-lightbox.show").css("overflow", "auto");
+	    $(".profile-lightbox.show").css("top", 10);
+	  }
+	}
+  $(this).attr("style", "");
+	swipedClosed = false; // reset swipedClosed
+	yCoord = null;
+	didSwipe = false;
+	$(".profile-lightbox").removeClass("disable-transition");
+}
 
 /*
 When the lightbox close button is clicked
@@ -65,19 +123,22 @@ close the lightbox
 
 Note, overflow-y to disable background scroll does not work on actual iOS devices. Works on desktop browsers.
 */
+function resetBackground() {
+	$("#lightbox-modal").css("opacity", "0");
+  setTimeout(function() {
+    // hide darkened background overlay after 800ms (when lightbox finishes transition)
+    $("#lightbox-modal").removeClass("show");
+  }, 800);
+  setTimeout(function() {
+    // re-enable window scrolling after 500ms (when lightbox exists screen)
+    $("body").css("overflow-y", "visible");
+  }, 500);
+}
+
 function hideLightbox(index) {
 	var prefix = "#kwlabs-container #who-we-are .item:eq("+index+") ";
 	$(prefix + ".profile-lightbox").addClass("hide").removeClass("show");
-	$("#lightbox-modal").css("opacity", "0");
-	// after the transition duration ends, remove show (pushes background modal backwards)
-	setTimeout(function() {
-		// hide darkened background overlay after 800ms (when lightbox finishes transition)
-		$("#lightbox-modal").removeClass("show");
-	}, 800);
-	setTimeout(function() {
-		// re-enable window scrolling after 500ms (when lightbox exists screen)
-		$("body").css("overflow-y", "visible");
-	}, 500);
+  resetBackground();
 };
 
 /*
@@ -120,8 +181,8 @@ function showLightbox(ele) {
 		$(prefix + ".lb-name").text(employees[index].name);
 		$(prefix + ".lb-role").text(employees[index].role);
 		$(prefix + ".lb-image").attr("src", employees[index].iconURL);
-		$(prefix + ".lb-bio:eq(0)").text(employees[index].biography1);
-		$(prefix + ".lb-bio:eq(1)").text(employees[index].biography2);
+		$(prefix + ".lb-bio:eq(0)").html(employees[index].biography1);
+		$(prefix + ".lb-bio:eq(1)").html(employees[index].biography2);
 
 		// set the close button to hide the lightbox on click
 		$(prefix + ".profile-lightbox span#lb-close").click(function(e) {
@@ -132,6 +193,10 @@ function showLightbox(ele) {
 			e.preventDefault();
 			hideLightbox(index);
 		});
+		
+    $(prefix + ".profile-lightbox").on('touchstart', handleTouchStart);
+    $(prefix + ".profile-lightbox").on('touchmove', handleTouchMove);
+    $(prefix + ".profile-lightbox").on('touchend', handleTouchEnd);
 	}
 	// set the lightbox to be visible (default)
 	// async delay to ensure the lightbox is rendered (so transitions can be performed)
@@ -179,12 +244,10 @@ $(document).ready(function() {
 		// set role text
 		$("#kwlabs-container #who-we-are .item:eq("+i+") .role").text(employees[i].role);
 		// set each icon to open lightbox on click
-    /*
     $("#kwlabs-container #who-we-are .item .open-lightbox:eq("+i+")").click(function(e) {
 			e.preventDefault();
 			showLightbox($(this));
 		});
-    */
 	}
 
 }); // end $(document).ready(...)
