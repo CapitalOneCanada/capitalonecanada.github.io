3864434fbd2f9d19fbccecbdf52ab4568b3eed74
Han, Ethan <Ethan.Han@capitalone.com>
2016-07-26 16:58:27 -0400

    Delaying moving-cloud initiation by 250ms to make sure the img tag is loaded.

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,38 +1,40 @@
 var startReset = [false, false, false, false];
 $(document).ready(function(){
 	// moving clouds
-	var durations = [14, 10, 6.5, 7.5]; // seconds
-	var pixelsPerSec = [0, 0, 0, 0]; // pixels per second ... beginning pos / duration
-	var endX = [0, 0, 0, 0];
-	$('img.cloud').each(function(i) {
-		$(this).css('transition-duration', durations[i] + "s");
-		endX[i] = $(this).width() * -1 - 100;
-		pixelsPerSec[i] = (parseInt($(this).css('right')) - endX[i]) / durations[i];
-		$(this).addClass('transition').css('right', endX[i]);
+	setTimeout(function() {
+		var durations = [14, 10, 6.5, 7.5]; // seconds
+		var pixelsPerSec = [0, 0, 0, 0]; // pixels per second ... beginning pos / duration
+		var endX = [0, 0, 0, 0];
+		$('img.cloud').each(function(i) {
+			$(this).css('transition-duration', durations[i] + "s");
+			endX[i] = $(this).width() * -1 - 100;
+			pixelsPerSec[i] = (parseInt($(this).css('right')) - endX[i]) / durations[i];
+			$(this).addClass('transition').css('right', endX[i]);
 
-		// start looping them back and forth
-		setLoopingTimeout(i);
-	});
+			// start looping them back and forth
+			setLoopingTimeout(i);
+		});
 
-	function setLoopingTimeout(i) {
-	  var internalCallback = function() {
-	    return function() {
-				var ele = $('img.cloud:eq('+i+')');
-				ele.css('transition-duration', "0s");
-				ele.css('right', window.innerWidth + ele.width() + 100); // put back to start position
-				setTimeout(function() { // give the processor enough time to put back to start position with disabled transition
-					if (!startReset[i]) {
-						durations[i] = (window.innerWidth + ele.width() + 100) / pixelsPerSec[i]; // new, longer duration based on pps speed
-						startReset[i] = true;
-					}
-					ele.css('transition-duration', durations[i] + "s"); // reset duration
-					ele.css('right', endX[i]);
-					window.setTimeout(internalCallback, durations[i] * 1000);
-				}, 150);
-	    }
-	  }();
-	  window.setTimeout(internalCallback, durations[i] * 1000);
-	};
+		function setLoopingTimeout(i) {
+		  var internalCallback = function() {
+		    return function() {
+					var ele = $('img.cloud:eq('+i+')');
+					ele.css('transition-duration', "0s");
+					ele.css('right', window.innerWidth + ele.width() + 100); // put back to start position
+					setTimeout(function() { // give the processor enough time to put back to start position with disabled transition
+						if (!startReset[i]) {
+							durations[i] = (window.innerWidth + ele.width() + 100) / pixelsPerSec[i]; // new, longer duration based on pps speed
+							startReset[i] = true;
+						}
+						ele.css('transition-duration', durations[i] + "s"); // reset duration
+						ele.css('right', endX[i]);
+						window.setTimeout(internalCallback, durations[i] * 1000);
+					}, 150);
+		    }
+		  }();
+		  window.setTimeout(internalCallback, durations[i] * 1000);
+		};
+	}, 250);
 
 	// open the menu
 	$('#main_nav .menu').click(function(e){
